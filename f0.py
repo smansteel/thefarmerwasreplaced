f95018418ed6faf86dac805a57ffb3a17eac72f2
sman_steel <smansteelyt@gmail.com>
2025-11-10 20:25:43 +0100

    fix goto

--- f0.py
+++ f0.py
@@ -6,38 +6,58 @@ def till_if_not_till():
         till()
 
 def goto_coord(x, y):
-    if(x > get_world_size()):
+    if(x >= get_world_size()):
         quick_print("x will be reduced to ", x%get_world_size(), " bc it is out of bounds")
-    if(y > get_world_size()):
+    if(y >= get_world_size()):
         quick_print("y will be reduced to ", y%get_world_size(), " bc it is out of bounds")
 
-    # quick_print("goto", x, y)
-    step = 1
-    if(get_pos_x() > x):
-        step = -1
-    for i in range(get_pos_x(), x%get_world_size(), step):
-        # quick_print(get_pos_x(), "wanted x :", x)
-        if(step == 1):
-            move(East)
-        else:
-            move(West)
+    x=x%get_world_size()
 
-    step = 1
-    if(get_pos_y() > y):
-        step = -1
-    for i in range(get_pos_y(), y%get_world_size(), step):
-        # quick_print(get_pos_y(), "wanted y :", y)
 
-        if(step == 1):
-            move(North)
+    if(x < get_pos_x()):
+        if(x<get_world_size()/4 and get_pos_x() > get_world_size()/4):
+            # print("case 1")
+            for i in range(0, get_world_size()-get_pos_x()+x):
+                move(East)
         else:
-            move(South)
+            for i in range(0, get_pos_x()-x):
+                move(West)
+    else:
+        if(x>get_world_size()/4 and get_pos_x() < get_world_size()/4):
+            # print("case 3")
+            for i in range(0, get_pos_x()+(get_world_size()-x)):
+                move(West)
+        else:
+            for i in range(0, x-get_pos_x()):
+                move(East)
+
+
+
+    if(y < get_pos_y()):
+        if(y<get_world_size()/4 and get_pos_y() > get_world_size()/4):
+            # print("case 1")
+            for i in range(0, get_world_size()-get_pos_y()+y):
+                move(North)
+        else:
+            for i in range(0, get_pos_y()-y):
+                move(South)
+    else:
+        if(y>get_world_size()/4 and get_pos_y() < get_world_size()/4):
+            # print("case 3")
+            for i in range(0, get_pos_y()+(get_world_size()-y)):
+                move(South)
+        else:
+            for i in range(0, y-get_pos_y()):
+                move(North)
+
+
 
 def get_z_pattern_list(start_x, start_y, size):
-    unchecked_list = []
+    list_pattern = []
 
     # z style patern to opti
     for i in range(start_y, start_y +size):
+        # quick_print(i)
         dir = i % 2
         if (dir == 0):
             start = 0
@@ -48,6 +68,35 @@ def get_z_pattern_list(start_x, start_y, size):
             end = -1
             step = -1
         for j in range(start_x+start, start_x+end, step):
+            # quick_print("    ", j)
             # quick_print((i,j))
-            unchecked_list.append((j, i))
-    return unchecked_list
+            list_pattern.append((j, i))
+    return list_pattern
+
+def plant_safe(entity):
+    if(entity == Entities.Grass):
+        plant(Entities.Grass)
+    elif(entity == Entities.Pumpkin):
+        quick_print("please use dedicated function")
+    elif(entity == Entities.Carrot):
+        till_if_not_till()
+        plant(Entities.Carrot)
+    else:
+        quick_print("not yet implemented")
+
+def plant_in_list(list, entity):
+    for item in list:
+        goto_coord(item[0], item[1])
+        plant_safe(entity)
+def harvest_in_list(list):
+    for item in list:
+        goto_coord(item[0], item[1])
+        harvest()
+
+def harvest_if_ready_and_plant_back(list):
+    for item in list:
+        goto_coord(item[0], item[1])
+        if(can_harvest()):
+            entt = get_entity_type()
+            harvest()
+            plant(entt)
--- main.py
+++ main.py
@@ -1,49 +1,50 @@
 import f0, f1
 from __builtins__ import *
+from f0 import goto_coord
 
 xsize = get_world_size()
 
 
 
 plant_map = [
-	[Entities.Grass, Entities.Tree, Entities.Carrot, Entities.Carrot, Entities.Tree, Entities.Carrot],
-	[Entities.Grass, Entities.Carrot, Entities.Carrot, Entities.Carrot, Entities.Carrot, Entities.Carrot],
-	[Entities.Grass, Entities.Tree, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin],
-	[Entities.Grass, Entities.Carrot, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin],
-	[Entities.Grass, Entities.Tree, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin],
-	[Entities.Tree, Entities.Grass, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin]
-			 ]
+    [Entities.Grass, Entities.Tree, Entities.Carrot, Entities.Carrot, Entities.Tree, Entities.Carrot],
+    [Entities.Grass, Entities.Carrot, Entities.Carrot, Entities.Carrot, Entities.Carrot, Entities.Carrot],
+    [Entities.Grass, Entities.Tree, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin],
+    [Entities.Grass, Entities.Carrot, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin],
+    [Entities.Grass, Entities.Tree, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin],
+    [Entities.Tree, Entities.Grass, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin, Entities.Pumpkin]
+             ]
 
 def get_plant_type_for_coord(x, y):
-	return plant_map[x][y]
+    return plant_map[x][y]
 
 
 def plant_prep(plant_to_plant):
-	if(plant_to_plant == Entities.Grass):
-		plant(Entities.Grass)
-	if(plant_to_plant == Entities.Tree):
-		plant(Entities.Tree)
-	if(plant_to_plant == Entities.Carrot):
-		if get_ground_type() != Grounds.Soil:
-			till()
-		plant(Entities.Carrot)
-	if(plant_to_plant == Entities.Pumpkin):
-		f0.till_if_not_till()
-		plant(Entities.Pumpkin)
+    if(plant_to_plant == Entities.Grass):
+        plant(Entities.Grass)
+    if(plant_to_plant == Entities.Tree):
+        plant(Entities.Tree)
+    if(plant_to_plant == Entities.Carrot):
+        if get_ground_type() != Grounds.Soil:
+            till()
+        plant(Entities.Carrot)
+    if(plant_to_plant == Entities.Pumpkin):
+        f0.till_if_not_till()
+        plant(Entities.Pumpkin)
 def tile_process(plant):
-	if(get_water() > 0.1):
-		use_item(Items.Water)
-	if can_harvest():
-		harvest()
-		plant_prep(plant)
-	elif get_entity_type() == Entities.Dead_Pumpkin:
-		harvest()
-		plant_prep(plant)
-	else:
-		plant_prep(plant)
+    if(get_water() > 0.1):
+        use_item(Items.Water)
+    if can_harvest():
+        harvest()
+        plant_prep(plant)
+    elif get_entity_type() == Entities.Dead_Pumpkin:
+        harvest()
+        plant_prep(plant)
+    else:
+        plant_prep(plant)
 
 def row_type():
-	tile_process(get_plant_type_for_coord(get_pos_x(), get_pos_y()))
+    tile_process(get_plant_type_for_coord(get_pos_x(), get_pos_y()))
 
 # reset
 clear()
@@ -59,9 +60,21 @@ quick_print("--- New Run ----")
 pumpkin_zone_x, pumpkin_zone_y = 0,0
 pumpkin_size = 5
 
-
+# f0.plant_in_list(f0.get_z_pattern_list(0,0,7), Entities.Carrot)
 # w
 while True:
-	f1.check_on_pumpkins(pumpkin_zone_x, pumpkin_zone_y, pumpkin_size)
+    goto_coord(0, 0)
+    harvest()
+    plant_prep(Entities.Carrot)
+    # print("")
+    goto_coord(4, 7)
+    harvest()
+    plant_prep(Entities.Carrot)
 
+    goto_coord(7, 4)
+    harvest()
+    plant_prep(Entities.Carrot)
 
+    # print("")
+
+    # f0.harvest_if_ready_and_plant_back(f0.get_z_pattern_list(0,0,7))
